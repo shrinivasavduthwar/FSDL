cadcd751c15a73c44c68d1eb4571a5da232b031d
Shrinivas Avduthwar <shrinivasavduthwar@gmail.com>
2026-03-30 14:59:44 +0530

    Added React portfolio project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Student Portfolio</p>
+    </footer>
+  );
+}
+export default Footer;
